ce434bbf8ced355bd925df78fb94672346d48484
JacoboDzul <jacobitodzul@gmail.com>
2024-04-12 18:15:30 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./Components/Login/Login";
+import DashBoard from "./Components/UserPanel/DashBoard/DashBoard";
+import ProtectedRoute from "./Components/Routes/ProtectedRoute";
+import AuthProvider from "./Auth/AuthProvider";
+import Registrar from "./Components/UserPanel/Registrar/Registrar";
+import Empleados from "./Components/UserPanel/Empleados/Empleado";
+import NuevoEmpleado from "./Components/UserPanel/Empleados/NuevoEmpleado";
+import Reservas from "./Components/Reservas/Reservaciones";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Reservas/>,
+  },
+  {
+    path: "/login",
+    element:<Login/>,
+  },
+  {
+    path:"/registrar",
+    element:<Registrar/>,
+  },
+  {
+    path: "/dashboard",
+    element: <DashBoard/>
+  },
+])
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <AuthProvider>
+     <RouterProvider router={router}/>
+    </AuthProvider>
+  </React.StrictMode>
+);
